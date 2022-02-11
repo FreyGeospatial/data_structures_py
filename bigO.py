4467b6db9488ff1342d76dc3950e2890a7f4774e
freygeospatial <jordan@freygeospatial.com>
2022-02-11 16:00:15 -0500

    added other examples of BigO notation

--- bigO.py
+++ bigO.py
@@ -25,7 +25,7 @@ def print_items(n):
 print_items(10)
 
 
-# 100 items -- n*n = n^2 = O(N^2)
+# ~100 items -- n*n = n^2 = O(N^2)
 def print_items(n):
     for i in range(n):
         for j in range(n):
@@ -33,7 +33,8 @@ def print_items(n):
 
 print_items(10)
 
-# ~1000 items -- n*n*n = n^3 = O(N^2) -- still exponential growth, so we simplify by dropping 3 and keeping 2
+# ~1000 items -- n*n*n = n^3 = O(N^2) -- still exponential growth, 
+# so we simplify by dropping 3 and keeping 2
 def print_items(n):
     for i in range(n):
         for j in range(n):
@@ -41,3 +42,78 @@ def print_items(n):
                 print(i, j, k)
 
 print_items(10)
+
+
+
+
+
+
+
+# O(n^2 + n) = O(n^2) as we drop non-dominants; the latter 'n' is inconsequential
+def print_items(n):
+    # n^2
+    for i in range(n):
+        for j in range(n):
+            print(i,j)
+        
+        # latter 'n'
+        for k in range(n):
+            print(i, j, k)
+
+print_items(10)
+
+# O(2) = O(1): number of operations remains constant regardless of value of n
+def add_items(n):
+    return n + n + n
+
+
+# O(N) + O(N) != O(2N) != O(N)
+# O(N) + O(N) == O(a + b)
+def print_items(a, b):
+    for i in range(a):
+        print(i)
+        
+    for j in range(b):
+        print(j)
+
+print_items(10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
