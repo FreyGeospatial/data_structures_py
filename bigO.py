42513130985f4de2875d28bb777c0350ad48e348
freygeospatial <jordan@freygeospatial.com>
2022-02-11 14:56:20 -0500

    added code to demonstrate linear bigO

---
+++ bigO.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# example of algorithm that is 0(n) (time to run is linear, or proportional to size)
+def print_items(n):
+    for i in range(n):
+        print(i)
+
+print_items(10)
+
